09e0c9fc42d8bc1abbfbb3501045f1bb7aed45ee
taylor <tailor@u.washington.edu>
2014-06-05 21:02:30 -0700

    Change everything to use just small test files

---
+++ Makefile
@@ -0,0 +1,2 @@
+t:
+	sh -x bup-practice.sh
--- bup-practice.sh
+++ bup-practice.sh
@@ -1,29 +1,26 @@
-if test -z "$DATA_DRIVE"
-then
-    echo set \$DATA_DRIVE first.  Quitting.
-    exit 1
-fi
+#!/bin/sh
 
-d=$(date +%Y%M%d%H%M%s)
+set -o errexit
 
-basedir=$(cygpath -u $DATA_DRIVE/$d)
+srcpath="$(pwd)/src dir"
+srcpath="$(pwd)/srcdir"
+srcdir="$(basename "$srcpath")"
 
-rm -rf $basedir
-mkdir -p $basedir
+mkdir -p "$srcdir"
+# wget -P "$srcdir" --timestamping http://installer-bin.streambox.com/mysql-essential-5.1.52-win32.msi
+# (cd "$srcdir" && split -a10 -b1m mysql-essential-5.1.52-win32.msi)
 
-rm -rf $basedir/d3cfrombkp
-time rsync -vaz /d/d3cfrombkp/ $basedir/d3cfrombkp
+rm -rf "$srcdir.bup"
 
-rm -rf  $basedir/d3cfrombkp.bup
-bup -d $basedir/d3cfrombkp.bup init
-bup -d $basedir/d3cfrombkp.bup index $basedir/d3cfrombkp
-bup -d $basedir/d3cfrombkp.bup save -n d3cfrombkp $basedir/d3cfrombkp
-bup -d $basedir/d3cfrombkp.bup ls --human-readable -a -l --human-readable d3cfrombkp
-bup -d $basedir/d3cfrombkp.bup restore -C $basedir/d3cfrombkp2
-GIT_DIR=$basedir/d3cfrombkp.bup git log
-GIT_DIR=$basedir/d3cfrombkp.bup git log d3cfrombkp
-du -sh  $basedir/d3cfrombkp.bup
-bup -d $basedir/d3cfrombkp.bup ls d3cfrombkp
-bup -d $basedir/d3cfrombkp.bup ls d3cfrombkp -v
-bup -d $basedir/d3cfrombkp.bup ls --human-readable d3cfrombkp
-bup -d $basedir/d3cfrombkp.bup ls --human-readable -a d3cfrombkp
+bup -d "$srcdir.bup" init
+bup -d "$srcdir.bup" index "$srcdir"
+bup -d "$srcdir.bup" save -n "$srcdir" "$srcdir"
+bup -d "$srcdir.bup" ls --human-readable -a -l --human-readable "$srcdir"
+bup -d "$srcdir.bup" restore -C "$srcdir"2
+GIT_DIR="$srcdir.bup" git log
+GIT_DIR="$srcdir.bup" git log "$srcdir"
+du -sh  "$srcdir.bup"
+bup -d "$srcdir.bup" ls "$srcdir"
+bup -d "$srcdir.bup" ls "$srcdir" -v
+bup -d "$srcdir.bup" ls --human-readable "$srcdir"
+bup -d "$srcdir.bup" ls --human-readable -a "$srcdir"
