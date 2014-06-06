a754baa3abf0b10d3ce32f61e5e906c98d04ec44
taylor <tailor@u.washington.edu>
2014-06-05 18:52:09 -0700

    use DATA_DRIVE instead of /tmp, because C: might be full

--- bup-practice.sh
+++ bup-practice.sh
@@ -1,16 +1,26 @@
-rm -rf /tmp/d3cfrombkp
-time rsync -vaz /d/d3cfrombkp/ /tmp/d3cfrombkp
+if test -z "$DATA_DRIVE"
+then
+    echo set \$DATA_DRIVE first.  Quitting.
+    exit 1
+fi
 
-rm -rf  /tmp/d3cfrombkp.bup
-bup -d /tmp/d3cfrombkp.bup init
-bup -d /tmp/d3cfrombkp.bup index /tmp/d3cfrombkp
-bup -d /tmp/d3cfrombkp.bup save -n d3cfrombkp /tmp/d3cfrombkp
-bup -d /tmp/d3cfrombkp.bup ls --human-readable -a -l --human-readable d3cfrombkp
-bup -d /tmp/d3cfrombkp.bup restore -C /tmp/d3cfrombkp2
-GIT_DIR=/tmp/d3cfrombkp.bup git log
-GIT_DIR=/tmp/d3cfrombkp.bup git log d3cfrombkp
-du -sh  /tmp/d3cfrombkp.bup
-bup -d /tmp/d3cfrombkp.bup ls d3cfrombkp
-bup -d /tmp/d3cfrombkp.bup ls d3cfrombkp -v
-bup -d /tmp/d3cfrombkp.bup ls --human-readable d3cfrombkp
-bup -d /tmp/d3cfrombkp.bup ls --human-readable -a d3cfrombkp
+d=$(date +%Y%M%d%H%M%s)
+
+basedir=$DATA_DRIVE/$d
+
+rm -rf $basedir/d3cfrombkp
+time rsync -vaz /d/d3cfrombkp/ $basedir/d3cfrombkp
+
+rm -rf  $basedir/d3cfrombkp.bup
+bup -d $basedir/d3cfrombkp.bup init
+bup -d $basedir/d3cfrombkp.bup index $basedir/d3cfrombkp
+bup -d $basedir/d3cfrombkp.bup save -n d3cfrombkp $basedir/d3cfrombkp
+bup -d $basedir/d3cfrombkp.bup ls --human-readable -a -l --human-readable d3cfrombkp
+bup -d $basedir/d3cfrombkp.bup restore -C $basedir/d3cfrombkp2
+GIT_DIR=$basedir/d3cfrombkp.bup git log
+GIT_DIR=$basedir/d3cfrombkp.bup git log d3cfrombkp
+du -sh  $basedir/d3cfrombkp.bup
+bup -d $basedir/d3cfrombkp.bup ls d3cfrombkp
+bup -d $basedir/d3cfrombkp.bup ls d3cfrombkp -v
+bup -d $basedir/d3cfrombkp.bup ls --human-readable d3cfrombkp
+bup -d $basedir/d3cfrombkp.bup ls --human-readable -a d3cfrombkp
